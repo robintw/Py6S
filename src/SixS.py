54dcbcf3354d2c3acfe066daa2d25098bd4f6017
Robin Wilson <robin@rtwilson.com>
2011-06-20 15:43:18 +0100

    Added extra file with classes to be used as Enums for 6S parameters. Added in capability to select atmospheric model type and aerosol model type.

--- src/SixS.py
+++ src/SixS.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 from Outputs import Outputs
+from SixSParams import *
 
 class SixS(object):
     """Wrapper for the 6S Radiative Transfer Model"""
@@ -14,6 +15,17 @@ class SixS(object):
     month = 7
     wavelength = 0.453
     aot550 = 0.5
+    ground_reflectance = 1.0
+    
+    aero_dustlike = 0
+    aero_water = 0
+    aero_oceanic = 0
+    aero_soot = 0
+    
+    atmos_profile = AtmosModel.MIDLATITUDE_SUMMER
+    aero_profile = AeroModel.MARITIME
+    
+    # Path to the SixS executable - filled in by the constructor
     sixs_path = ""
     
     # Stores the outputs from 6S as an instance of the Outputs class
@@ -39,25 +51,71 @@ class SixS(object):
                 return os.path.join(path, program)
         return None
         
-    def write_input_file(self):
-        """Writes the generated 6S input file to a temporary file"""
-        f = open("tmp_in.txt", "w")
-        f.write("0 (User defined)\n")
-        f.write('%d %d %d %d %d %d\n' % (self.solar_z, self.solar_a, self.view_z, self.view_a, self.month, self.day))
-        f.write("""2 Midlatitude Summer
-2 Maritime Model
-0\n""")
-        f.write('%f value\n' % self.aot550)
-        f.write("""0 (target level)
-0 (sensor level)
--1 monochromatic\n""")
-        f.write('%f \n' % self.wavelength)
-        f.write("""0 Homogeneous surface
+
+    def create_geom_lines(self):
+        """Create the line for the input file specifying the geometeries and the day/month"""
+        return '0 (User defined)\n%d %d %d %d %d %d\n' % (self.solar_z, self.solar_a, self.view_z, self.view_a, self.month, self.day)
+
+    def create_atmos_aero_lines(self):
+        # As long as we've selected one of the pre-specified aerosol models
+        # (ie. not the user one) then simply return the numbers
+        if self.aero_profile != AeroModel.USER:
+            return """%d
+%d\n""" % (self.atmos_profile, self.aero_profile)
+        # Otherwise, check we've been given all of the parameters and put them in
+        else:
+            if self.aero_dustlike + self.aero_oceanic + self.aero_soot + self.aero_water != 1.0:
+                print "Incorrect specification of User-defined Aerosol Components: Must add up to 1.0"
+                return ""
+            return """%d
+%d
+%f %f %f %f""" % (self.atmos_profile, self.aero_profile, )
+            
+
+    def create_aot_vis_lines(self):
+        # If aot is set then use it
+        if self.aot550 != None:
+            return """0
+%f value\n""" % self.aot550
+            
+    def create_elevation_lines(self):
+        return """0 (target level)
+0 (sensor level)\n"""
+
+    def create_wavelength_lines(self):
+        return """-1 monochromatic
+%f\n""" % self.wavelength
+
+    def create_surface_lines(self):
+        return """0 Homogeneous surface
 0 No directional effects
 0 constant value for ro
-1.0
--1 No atm. corrections selected
-""")
+%f\n""" % self.ground_reflectance
+
+    def create_atmos_corr_lines(self):
+        return """-1 No atm. corrections selected\n"""
+
+    def write_input_file(self):
+        """Generates a 6S input file from the parameters stored in the object
+        and writes it12"""
+        
+        f = open("tmp_in.txt", "w")
+        
+        input_file = self.create_geom_lines()
+        
+        input_file += self.create_atmos_aero_lines()
+        
+        input_file += self.create_aot_vis_lines()
+        
+        input_file += self.create_elevation_lines()
+        
+        input_file += self.create_wavelength_lines()
+        
+        input_file += self.create_surface_lines()
+
+        input_file += self.create_atmos_corr_lines()
+
+        f.write(input_file)
 
     def run(self):
         """Runs the 6S model and stores the output in the output variable"""
@@ -68,6 +126,9 @@ class SixS(object):
         process = subprocess.Popen("%s < tmp_in.txt" % self.sixs_path, shell=True, stdout=subprocess.PIPE)
         self.outputs = Outputs(process.communicate()[0])
 
+
+
+
 # If this file is run itself then print output showing which sixs executable will be used
 if __name__ == "__main__":
     test = SixS()
@@ -75,4 +136,5 @@ if __name__ == "__main__":
     sixs_path = test.find_path("sixs")
     if sixs_path == None:
         print "Error: cannot find sixs executable in $PATH or current directory."
-    print "Using 6S located at %s" % sixs_path
+    else:
+        print "Using 6S located at %s" % sixs_path
--- src/Test.py
+++ src/Test.py
@@ -1,8 +1,13 @@
 from SixS import SixS
+from SixSParams import *
 
 test = SixS()
+
+test.aero_profile = AeroModel.USER
 
 test.write_input_file()
 test.run()
 
 print test.outputs.fulltext
+
+print test.outputs.irradiance_direct
