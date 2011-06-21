ec5503d7319dbee637e11afea7c932b62b6709c2
Robin Wilson <robin@rtwilson.com>
2011-06-21 20:09:27 +0100

    Updated plotting routine to use updated API - eg. not calling write_input_file(), and using updated names for outputs

--- src/GetSpectra.py
+++ src/GetSpectra.py
@@ -1,5 +1,6 @@
 import numpy as np
 from SixS import SixS
+import matplotlib.pyplot as plt
 
 test = SixS()
 
@@ -9,19 +10,18 @@ diffuse_res = []
 
 for wv in np.arange(0.4, 0.8, 0.001):
     test.wavelength = wv
-    test.write_input_file()
     test.run()
     
     
     wavelengths.append(wv)
-    direct_res.append(test.outputs.irradiance_direct)
-    diffuse_res.append(test.outputs.irradiance_diffuse)
+    direct_res.append(test.outputs.direct_solar_irradiance)
+    diffuse_res.append(test.outputs.diffuse_solar_irradiance)
     
 print direct_res
-#plt.plot(wavelengths, direct_res, "r", label="Direct")
-#plt.plot(wavelengths, diffuse_res, "b", label="Diffuse")
-#plt.xlabel("Wavelength (micrometres)")
-#plt.ylabel("Irradiance (W/m2/mic)")
-#plt.title("View Zenith: 23, View Azimuth: 190")
-#plt.legend()
-#plt.savefig("FirstOutput_a.png")
+plt.plot(wavelengths, direct_res, "r", label="Direct")
+plt.plot(wavelengths, diffuse_res, "b", label="Diffuse")
+plt.xlabel("Wavelength (micrometres)")
+plt.ylabel("Irradiance (W/m2/mic)")
+plt.title("View Zenith: 23, View Azimuth: 190")
+plt.legend()
+plt.savefig("SecondOutput.png")
