1f46300e41a765a0a108303485159a17174cbd0c
Robin Wilson <robin@rtwilson.com>
2011-06-21 20:21:19 +0100

    Updated Test routine to work with latest version

--- src/Test.py
+++ src/Test.py
@@ -1,12 +1,26 @@
 from SixS import SixS
 from SixSParams import *
+import yaml
+import pprint
 
 test = SixS()
+#
+#SixS.save_params(test, "params_output_2.yml")
+#test = test.load_params("params_output.yml")
+#print test.aero_soot
+
+
+#f = open("test.yaml", "r")
+#obj = yaml.load(f)
+#print obj.aero_profile
+#test.run()
+#print test.outputs.direct_solar_irradiance
 
 test.aero_profile = AeroModel.MARITIME
-test.write_input_file()
+
 test.run()
 
-print test.outputs.fulltext
 
-print test.outputs.irradiance_direct
+#print test.outputs.fulltext
+
+#print test.outputs.irradiance_direct
