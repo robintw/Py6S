7276d586fcb1b252f0839b9677550bd2eee81c05
Robin Wilson <robin@rtwilson.com>
2011-06-22 14:16:31 +0100

    Added type conversion to Outputs - ensuring all outputs appear as the correct type (string, int, float etc). Added safe to_int function to deal with int(5.00) giving an error.

--- src/Outputs.py
+++ src/Outputs.py
@@ -38,23 +38,25 @@ class Outputs(object):
         # to the first value in the tuple is found. If this is CURRENT (ie. 0) then it is the line on whic
         # the text was found, if it is 1 then it is the next line, 2 the one after that etc.
         # The next item in the tuple is the index of the split line to extract the value from, and the
-        # final item is the key to store it in in the values dictionary.
+        # third item is the key to store it in in the values dictionary. The final item is the type to convert
+        # it to - the type conversion function must be specified. More specific functions such as math.floor can
+        # be used here if desired.
         
-        #              Search Term             Line   Index DictKey   
-        extractors = { "solar zenith angle" : (CURRENT, 3, "solar_z"),
-                       "ground pressure" : (CURRENT, 3, "ground_pressure"),                  
-                       "irr. at ground level" : (2, 0, "direct_solar_irradiance"),
-                       "irr. at ground level (w/" : (2, 1, "diffuse_solar_irradiance"),
-                       "irr. at ground level (w/m2/mic)" : (2, 2, "environmental_irradiance"),
-                       "% of irradiance" : (2, 0, "percent_direct_solar_irradiance"),
-                       "% of irradiance at" : (2, 1, "percent_diffuse_solar_irradiance"),
-                       "% of irradiance at ground level" : (2, 2, "percent_environmental_irradiance"),
-                       "sol. spect (in w/m2/mic)" : (1, 0, "solar_spectrum"),
-                       "scattering angle:" : (CURRENT, 2, "scattering_angle"),
-                       "azimuthal angle difference:" : (CURRENT, 7, "azimuthal_angle_difference"),
-                       "visibility :" : (CURRENT, 2, "visibility"),
-                       "opt. thick. 550 nm :" : (CURRENT, 9, "aot550"),
-                       "apparent reflectance" : (CURRENT, 2, "integrated_apparent_reflectance")
+        #              Search Term             Line   Index DictKey   Type
+        extractors = { "solar zenith angle" : (CURRENT, 3, "solar_z", self.to_int),
+                       "ground pressure" : (CURRENT, 3, "ground_pressure", float),                  
+                       "irr. at ground level" : (2, 0, "direct_solar_irradiance", float),
+                       "irr. at ground level (w/" : (2, 1, "diffuse_solar_irradiance", float),
+                       "irr. at ground level (w/m2/mic)" : (2, 2, "environmental_irradiance", float),
+                       "% of irradiance" : (2, 0, "percent_direct_solar_irradiance", float),
+                       "% of irradiance at" : (2, 1, "percent_diffuse_solar_irradiance", float),
+                       "% of irradiance at ground level" : (2, 2, "percent_environmental_irradiance", float),
+                       "sol. spect (in w/m2/mic)" : (1, 0, "solar_spectrum", float),
+                       "scattering angle:" : (CURRENT, 2, "scattering_angle", float),
+                       "azimuthal angle difference:" : (CURRENT, 7, "azimuthal_angle_difference", float),
+                       "visibility :" : (CURRENT, 2, "visibility", float),
+                       "opt. thick. 550 nm :" : (CURRENT, 9, "aot550", float),
+                       "apparent reflectance" : (CURRENT, 2, "integrated_apparent_reflectance", float)
                       }
                 
         for index in range(len(lines)):
@@ -68,10 +70,14 @@ class Outputs(object):
                     # Otherwise, work out which line to use and get it
                     else:
                         extracting_line = lines[index + details[0]]
-                        
+                    
+                    funct = details[3]
                     items = extracting_line.split()
-                    self.values[details[2]] = items[details[1]]
+                    self.values[details[2]] = funct(items[details[1]])
                     
         pp = pprint.PrettyPrinter(indent=4)
          
         pp.pprint(self.values)
+        
+    def to_int(self, str):
+        return int(float(str))
