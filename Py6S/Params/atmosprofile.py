bdcc0fccc5519072bbd206ac55373246454ac75d
Robin Wilson <robin@rtwilson.com>
2011-07-03 18:29:56 +0100

    From previous commit - added new files

---
+++ Py6S/Params/atmosprofile.py
@@ -0,0 +1,9 @@
+class AtmosProfile:
+    """Stores a enumeration for the pre-specified atmospheric model types"""
+    NO_GASEOUS_ABSORPTION=0 
+    TROPICAL=1
+    MIDLATITUDE_SUMMER=2
+    MIDLATITUDE_WINTER=3
+    SUBARCTIC_SUMMER=4
+    SUBARCTIC_WINTER=5
+    US_STANDARD_1962=6
