fae1807bacfa8bf268b421e1accf4e1909b1f140
Robin Wilson <robin@rtwilson.com>
2012-02-02 11:06:02 +0000

    Significant work on LUT stuff - now basically works with dictionary input and setting parameters (even s.param1.param2) properly.

--- Py6S/lut_test.py
+++ Py6S/lut_test.py
@@ -1,5 +1,7 @@
 import itertools
 import numpy as np
+import sixs
+from Params import *
 
 def test(i):
   for res in i:
@@ -10,6 +12,26 @@ def named_product(**items):
     vals = items.values()
     for res in itertools.product(*vals):
         yield dict(zip(names, res))
+        
+def named_product_from_dict(d):
+    names = d.keys()
+    vals = d.values()
+    for res in itertools.product(*vals):
+        yield dict(zip(names, res))
+        
+def set_attrs_from_dict(sixs, d):
+  for key, value in d.iteritems():
+    if "." in key:
+      # We've got a parameter which isn't just s.param but s.param.param
+      # For example, s.geometry.solar_z
+      s = key.split(".")
+      obj = getattr(sixs, s[0])
+      attrib = s[1]
+    else:
+      # We have a simple attribute
+      obj = sixs
+      attrib = key
+    setattr(obj, attrib, value)
 
 
 i = named_product(a = "12", b = "ab")
@@ -18,9 +40,28 @@ test(i)
 solar_z = np.arange(0, 70, 10)
 solar_a = np.arange(0, 360, 45)
 
-i = named_product(solar_z = solar_z, solar_a = solar_a)
-test(i)
+i = named_product_from_dict({'geometry.view_z' : solar_z, 'geometry.view_a' : solar_a})
+
+s = sixs.SixS()
+s.aero_profile = AeroProfile.Urban
+s.ground_reflectance = GroundReflectance.HomogeneousRoujean(0.037, 0.0, 0.133)
+s.geometry = GeometryUser()
+s.geometry.solar_z = 30
+s.geometry.solar_a = 0
+
+for params in i:
+  #print params
+  set_attrs_from_dict(s, params)
+  s.run()
+  print s.outputs.pixel_reflectance
 
 
-#def create_lut()
 #  [GroundReflectance.HomogeneousLambertian(x) for x in np.arange(0, 10)]
+
+# Creates a lookup table by running 6S with all the combinations of the parameters given.
+# Parameters:
+# 1: SixS object with the standard parameters you want
+# 2: Dictionary with keys equal to the parameter name (the bit after the s. when setting it), and values an iterable with
+# the range of values wanted
+#def create_lut(s, dict):
+  
