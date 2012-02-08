55d877b649f2ccbe237836cdf58a3eb323d79972
Robin Wilson <robin@rtwilson.com>
2012-02-08 22:11:25 +0000

    Added docstrings to all AeroProfile methods describing their usage

--- Py6S/Params/aeroprofile.py
+++ Py6S/Params/aeroprofile.py
@@ -7,22 +7,28 @@ class AeroProfile:
 
     @classmethod
     def NoAerosol(cls):
+      """No aerosols"""
       return "0 (No Aerosol)"
 
     @classmethod
     def Continental(cls):
+      """Continental aerosol profile"""
       return "1 (Continental)"
 
     @classmethod
     def Maritime(cls):
+      """Maritime aerosol profile"""
       return "2 (Maritime)"
       
     @classmethod      
     def Urban(cls):
+      """Urban aerosol profile"""
       return "3 (Urban)"
     
     @classmethod
     def User(cls, **kwargs):
+        """User specified aerosol profile, as a mixture of pre-defined components. Call using keyword arguments for the
+        components of: dust, water, oceanic and soot. For example AeroProfile.User(dust=0.3, oceanic=0.7)."""
         d = defaultdict(lambda: 0, kwargs)
         
         dust = d['dust']
@@ -37,12 +43,15 @@ class AeroProfile:
 
     @classmethod
     def Desert(cls):
+      """Desert aerosol profile"""
       return "5 (Desert)"
 
     @classmethod
     def BiomassBurning(cls):
+      """Biomass Burning aerosol profile"""
       return "6 (Biomass Burning)"
 
     @classmethod
     def Stratospheric(cls):
+      """Stratospheric aerosol profile"""
       return "7 (Stratospheric)"
