aef41dfe6499871adbb88e28e42c4d67615c29ac
Robin Wilson <robin@rtwilson.com>
2012-02-08 12:28:43 +0000

    Added all other geometries - GOES E/W, AVHRR AM/PM, SPOT HRV and Landsat TM

--- Py6S/Params/geometry.py
+++ Py6S/Params/geometry.py
@@ -20,3 +20,66 @@ class GeometryMeteosat:
   
   def __str__(self):
     return '1 (Meteosat)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.column, self.line)
+    
+class GeometryGoesEast:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  column = 0
+  line = 0
+  
+  def __str__(self):
+    return '2 (Goes East)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.column, self.line)
+    
+    
+class GeometryGoesWest:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  column = 0
+  line = 0
+  
+  def __str__(self):
+    return '3 (Goes West)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.column, self.line)
+    
+class GeometryAVHRR_PM:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  column = 0
+  ascendant_node_longitude = 0
+  ascendant_node_hour = 0
+  
+  def __str__(self):
+    return '4 (AVHRR PM NOAA)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.column, self.ascendant_node_longitude, self.ascendant_node_hour)
+    
+class GeometryAVHRR_AM:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  column = 0
+  ascendant_node_longitude = 0
+  ascendant_node_hour = 0
+  
+  def __str__(self):
+    return '5 (AVHRR AM NOAA)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.column, self.ascendant_node_longitude, self.ascendant_node_hour)
+    
+class GeometrySPOT_HRV:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  longitude = 0
+  latitude = 0
+  
+  def __str__(self):
+    return '6 (SPOT)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.longitude, self.latitude)
+    
+class GeometryLandsat_TM:
+  month = 1
+  day = 1
+  gmt_decimal_hour = 0
+  longitude = 0
+  latitude = 0
+  
+  def __str__(self):
+    return '7 (TM)\n%d %d %d %d %d (Geometrical Conditions)' % (self.month, self.day, self.gmt_decimal_hour, self.longitude, self.latitude)
