141a6b38fbd0a7aa27c0ef30ec0ecf8d1595702f
Robin Wilson <robin@rtwilson.com>
2012-02-08 22:09:07 +0000

    Converted all AeroProfile class entries from simple constants to Class Methods. Added fancy function for User Aerosol Profile using keyword arguments and then parsing into dictionary.

--- Py6S/Params/aeroprofile.py
+++ Py6S/Params/aeroprofile.py
@@ -1,10 +1,48 @@
+from collections import defaultdict
+from sixs_exceptions import *
+
+
 class AeroProfile:
     """Stores an enumeration for the pre-specified aerosol model types."""
-    NoAerosol=0
-    Continental=1
-    Maritime=2
-    Urban=3
-    User=4
-    Desert=5
-    BiomassBurning=6
-    Stratospheric=7
+
+    @classmethod
+    def NoAerosol(cls):
+      return "0 (No Aerosol)"
+
+    @classmethod
+    def Continental(cls):
+      return "1 (Continental)"
+
+    @classmethod
+    def Maritime(cls):
+      return "2 (Maritime)"
+      
+    @classmethod      
+    def Urban(cls):
+      return "3 (Urban)"
+    
+    @classmethod
+    def User(cls, **kwargs):
+        d = defaultdict(lambda: 0, kwargs)
+        
+        dust = d['dust']
+        water = d['water']
+        oceanic = d['oceanic']
+        soot = d['soot']
+        
+        if (((dust + water + oceanic + soot) - 1) > 0.01):
+          raise ParameterError("Aerosol Profile", "User aerosol components don't sum to 1.0")
+        
+        return "4 (User's Components)\n%f, %f, %f, %f" % (dust, water, oceanic, soot)
+
+    @classmethod
+    def Desert(cls):
+      return "5 (Desert)"
+
+    @classmethod
+    def BiomassBurning(cls):
+      return "6 (Biomass Burning)"
+
+    @classmethod
+    def Stratospheric(cls):
+      return "7 (Stratospheric)"
