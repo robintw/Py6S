b5fdef6aff3c64c6805f22e9cd1ec6786bbb694c
Robin Wilson <robin@rtwilson.com>
2012-02-13 14:19:55 +0000

    Added method to parameterise with radiosonde profile to AtmosProfile

--- Py6S/Params/atmosprofile.py
+++ Py6S/Params/atmosprofile.py
@@ -1,3 +1,5 @@
+from sixs_exceptions import *
+
 class AtmosProfile:
     """Stores a enumeration for the pre-specified atmospheric model types"""
     NoGaseousAbsorption=0 
@@ -12,8 +14,32 @@ class AtmosProfile:
     def PredefinedType(cls, type):
       """Use a predefined atmosphere type, one of the constants defined in this class"""
       return "%d" % type
-      
+    
+    @classmethod
     def UserWaterAndOzone(cls, water, ozone):
       """Use an atmosphere defined by an amount of water vapour (in g/cm^2) and ozone (in cm-atm)"""
       return "8 (Water Vapour and Ozone)\n%f %f" % (water, ozone)
+    
+    @classmethod
+    def RadiosondeProfile(cls, data):
+      """Use an atmosphere defined by a profile from a radiosonde measurement. The data argument must be a dictionary with the
+      following keys, each of which must be an iterable containing the data:
+      altitude
+      pressure
+      temperature
+      water
+      ozone
       
+      There must be 34 items in each iterable"""
+      
+      # Check to make sure all iterables have 34 items
+      all_lists = [data['altitude'], data['pressure'], data['temperature'], data['water'], data['ozone']]
+      if not all(len(x) == 34 for x in all_lists):
+        raise ParameterError("radiosonde levels", "There must be 34 values in the lists for each radiosonde attribute (altitude, pressure, temperature, water, ozone)")
+      
+      result = ""
+        
+      for i in range(34):
+        result = result + "%f %f %f %f %f\n" % (data['altitude'][i], data['pressure'][i], data['temperature'][i], data['water'][i], data['ozone'][i])
+      
+      return result
