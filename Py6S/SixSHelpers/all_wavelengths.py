7daed3d38869893556e137db36adfad68ab6326c
Robin Wilson <robin@rtwilson.com>
2012-02-27 10:15:38 +0000

    Added docstrings to Wavelengths helper class.

--- Py6S/SixSHelpers/all_wavelengths.py
+++ Py6S/SixSHelpers/all_wavelengths.py
@@ -3,9 +3,33 @@ import numpy as np
 from matplotlib.pyplot import *
 
 class Wavelengths:
+  """Helper functions for running the 6S model for a range of wavelengths, and plotting the result"""
   
   @classmethod
   def run_for_wavelengths(cls, s, wavelengths, output_name=None):
+    """Runs the given SixS parameterisation for each of the wavelengths given, optionally extracting a specific output.
+    
+    Arguments:
+    
+     * ``s`` -- A SixS instance with the parameters set as required
+     * ``wavelengths`` -- An iterable containing the wavelengths to iterate over
+     * ``output_name`` -- (Optional) The output to extract from ``s.outputs``, as a string that could be placed after ``s.outputs.``, for example ``pixel_reflectance``
+    
+    Return value:
+    
+    A tuple containing the wavelengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
+    or a list of values of the selected output if ``output_name`` is set.
+    
+    Example usage::
+    
+      # Run for all wavelengths from 0.4 to 0.5 micrometers, with a spacing of 1nm, returns SixS.Outputs instances
+      wavelengths, results = SixSHelpers.Wavelengths.run_for_wavelengths(s, np.arange(0.400, 0.500, 0.001))
+      # Run for all wavelengths from 0.4 to 0.5 micrometers, with a spacing of 1nm, returns a list of pixel radiance values
+      wavelengths, results = SixSHelpers.Wavelengths.run_for_wavelengths(s, np.arange(0.400, 0.500, 0.001), output_name='pixel_radiance')
+      # Run for the first three Landsat TM bands
+      wavelengths, results = SixSHelpers.Wavelengths.run_for_wavelengths(s, [Wavelength.LANDSAT_TM_B1, Wavelength.LANDSAT_TM_B2, Wavelength.LANDSAT_TM_B3)
+    
+    """
     results = []
     
     for wavelength in wavelengths:
@@ -20,16 +44,58 @@ class Wavelengths:
 
   @classmethod
   def run_vnir(cls, s, spacing=0.005, output_name=None):
+    """Runs the given SixS parameterisation for wavelengths over the Visible-Near Infrared range, optionally extracting a specific output.
+    
+    By default, the given model is run for wavelengths from 0.4-1.4um, with a spacing of 5nm.
+    
+    Arguments:
+    
+     * ``s`` -- A SixS instance with the parameters set as required
+     * ``spacing`` -- (Optional) The spacing to use between each wavelength, in um. Eg. a spacing of 0.001 is a spacing of 1nm.
+     * ``output_name`` -- (Optional) The output to extract from ``s.outputs``, as a string that could be placed after ``s.outputs.``, for example ``pixel_reflectance``
+    
+    Return value:
+    
+    A tuple containing the wavelengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
+    or a list of values of the selected output if ``output_name`` is set.
+    
+    Example usage::
+    
+      # Run for the VNIR wavelengths, with the default spacing (5nm), returning SixS.Outputs instances
+      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s)
+      # Run for the VNIR wavelengths, with a spacing of 10nm, returning pixel reflectance values
+      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
+      
+    """
+    
     wv = np.arange(0.400, 1.400, spacing)
     return cls.run_for_wavelengths(s, wv, output_name=output_name)
   
   @classmethod
   def run_whole_range(cls, s, spacing=0.010, output_name=None):
+    """Runs the given SixS parameterisation for the entire wavelength range of the 6S model, optionally extracting a specific output.
+    
+    By default, the given model is run for wavelengths from 0.2-4.0um, with a spacing of 10nm.
+    
+    Arguments:
+    
+     * ``s`` -- A SixS instance with the parameters set as required
+     * ``spacing`` -- (Optional) The spacing to use between each wavelength, in um. Eg. a spacing of 0.001 is a spacing of 1nm.
+     * ``output_name`` -- (Optional) The output to extract from ``s.outputs``, as a string that could be placed after ``s.outputs.``, for example ``pixel_reflectance``
+    
+    Return value:
+    
+    A tuple containing the wavelengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
+    or a list of values of the selected output if ``output_name`` is set.
+    
+    Example usage::
+    
+      # Run for the VNIR wavelengths, with the default spacing (5nm), returning SixS.Outputs instances
+      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s)
+      # Run for the VNIR wavelengths, with a spacing of 10nm, returning pixel reflectance values
+      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
+      
+    """
     wv = np.arange(0.2, 4.0, spacing)
     return cls.run_for_wavelengths(s, wv, output_name=output_name)
-  
-  @classmethod
-  def plot_wavelengths(cls, wavelengths, values):
-    plot(wavelengths, values)
-    xlabel("Micrometres")
     
