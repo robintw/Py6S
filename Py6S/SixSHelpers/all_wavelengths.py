1585946800f2b711a50202e1826792b6ceb07190
Robin Wilson <robin@rtwilson.com>
2012-03-01 21:45:43 +0000

    Fixed major bugs with running for different sensors in All Wavelengths - lists were wrong sizes, wrong constants etc.

--- Py6S/SixSHelpers/all_wavelengths.py
+++ Py6S/SixSHelpers/all_wavelengths.py
@@ -23,11 +23,11 @@ class Wavelengths:
     Example usage::
     
       # Run for all wavelengths from 0.4 to 0.5 micrometers, with a spacing of 1nm, returns SixS.Outputs instances
-      wavelengths, results = SixSHelpers.Wavelengths.run_wavelengths(s, np.arange(0.400, 0.500, 0.001))
+      wavelengths, results = SixSHelpers.Wavelength.run_wavelengths(s, np.arange(0.400, 0.500, 0.001))
       # Run for all wavelengths from 0.4 to 0.5 micrometers, with a spacing of 1nm, returns a list of pixel radiance values
-      wavelengths, results = SixSHelpers.Wavelengths.run_wavelengths(s, np.arange(0.400, 0.500, 0.001), output_name='pixel_radiance')
+      wavelengths, results = SixSHelpers.Wavelength.run_wavelengths(s, np.arange(0.400, 0.500, 0.001), output_name='pixel_radiance')
       # Run for the first three Landsat TM bands
-      wavelengths, results = SixSHelpers.Wavelengths.run_wavelengths(s, [Wavelength.LANDSAT_TM_B1, Wavelength.LANDSAT_TM_B2, Wavelength.LANDSAT_TM_B3)
+      wavelengths, results = SixSHelpers.Wavelength.run_wavelengths(s, [Wavelength.LANDSAT_TM_B1, Wavelength.LANDSAT_TM_B2, Wavelength.LANDSAT_TM_B3)
     
     """
     results = []
@@ -62,9 +62,9 @@ class Wavelengths:
     Example usage::
     
       # Run for the VNIR wavelengths, with the default spacing (5nm), returning SixS.Outputs instances
-      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s)
+      wavelengths, results = SixSHelpers.Wavelength.run_vnir(s)
       # Run for the VNIR wavelengths, with a spacing of 10nm, returning pixel reflectance values
-      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
+      wavelengths, results = SixSHelpers.Wavelength.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
       
     """
     
@@ -92,9 +92,9 @@ class Wavelengths:
     Example usage::
     
       # Run for the VNIR wavelengths, with the default spacing (5nm), returning SixS.Outputs instances
-      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s)
+      wavelengths, results = SixSHelpers.Wavelength.run_vnir(s)
       # Run for the VNIR wavelengths, with a spacing of 10nm, returning pixel reflectance values
-      wavelengths, results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
+      wavelengths, results = SixSHelpers.Wavelength.run_vnir(s, spacing=0.010, output_name='pixel_reflectance')
       
     """
     wv = np.arange(0.2, 4.0, spacing)
@@ -115,7 +115,7 @@ class Wavelengths:
     or a list of values of the selected output if ``output_name`` is set.
     """
     
-    wv = [Wavelengths.LANDSAT_TM_B1, Wavelengths.LANDSAT_TM_B2, Wavelengths.LANDSAT_TM_B3, Wavelengths.LANDSAT_TM_B4, Wavelengths.LANDSAT_TM_B5, Wavelengths.LANDSAT_TM_B7]
+    wv = [Wavelength.LANDSAT_TM_B1, Wavelength.LANDSAT_TM_B2, Wavelength.LANDSAT_TM_B3, Wavelength.LANDSAT_TM_B4, Wavelength.LANDSAT_TM_B5, Wavelength.LANDSAT_TM_B7]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.485, 0.56, 0.66, 0.83, 1.65, 2.215]
@@ -136,7 +136,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.ETM_B1, Wavelengths.ETM_B2, Wavelengths.ETM_B3, Wavelengths.ETM_B4, Wavelengths.ETM_B5, Wavelengths.ETM_B7]
+    wv = [Wavelength.ETM_B1, Wavelength.ETM_B2, Wavelength.ETM_B3, Wavelength.ETM_B4, Wavelength.ETM_B5, Wavelength.ETM_B7]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.4825, 0.565, 0.66, 0.825, 1.65, 2.215]
@@ -157,7 +157,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.LANDSAT_MSS_B1, Wavelengths.LANDSAT_MSS_B2, Wavelengths.LANDSAT_MSS_B3, Wavelengths.LANDSAT_MSS_B4]
+    wv = [Wavelength.LANDSAT_MSS_B1, Wavelength.LANDSAT_MSS_B2, Wavelength.LANDSAT_MSS_B3, Wavelength.LANDSAT_MSS_B4]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.55, 0.65, 0.75, 0.95]
@@ -178,7 +178,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.MERIS_B1, Wavelengths.MERIS_B2, Wavelengths.MERIS_B3, Wavelengths.MERIS_B4, Wavelengths.MERIS_B5, Wavelengths.MERIS_B6, Wavelengths.MERIS_B7, Wavelengths.MERIS_B8, Wavelengths.MERIS_B9, Wavelengths.MERIS_B10, Wavelengths.MERIS_B11, Wavelengths.MERIS_B12, Wavelengths.MERIS_B14, Wavelengths.MERIS_B15]
+    wv = [Wavelength.MERIS_B1, Wavelength.MERIS_B2, Wavelength.MERIS_B3, Wavelength.MERIS_B4, Wavelength.MERIS_B5, Wavelength.MERIS_B6, Wavelength.MERIS_B7, Wavelength.MERIS_B8, Wavelength.MERIS_B9, Wavelength.MERIS_B10, Wavelength.MERIS_B11, Wavelength.MERIS_B12, Wavelength.MERIS_B13,Wavelength.MERIS_B14, Wavelength.MERIS_B15]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.4125, 0.4425, 0.490, 0.510, 0.560, 0.620, 0.665, 0.8125, 0.70875, 0.75375, 0.760625, 0.77875, 0.865, 0.885, 0.900]
@@ -199,10 +199,10 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.MODIS_B1, Wavelengths.MODIS_B2, Wavelengths.MODIS_B3, Wavelengths.MODIS_B4, Wavelengths.MODIS_B5, Wavelengths.MODIS_B6, Wavelengths.MODIS_B7, Wavelengths.MERIS_B8]
+    wv = [Wavelength.MODIS_B8, Wavelength.MODIS_B1, Wavelength.MODIS_B2, Wavelength.MODIS_B3, Wavelength.MODIS_B4, Wavelength.MODIS_B5, Wavelength.MODIS_B6, Wavelength.MODIS_B7]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
-    centre_wvs = [0.645, 0.8585, 0.469, 0.555, 1.24, 1.64, 2.13]
+    centre_wvs = [0.4125, 0.645, 0.8585, 0.469, 0.555, 1.24, 1.64, 2.13]
     
     return (centre_wvs, res)
     
@@ -220,7 +220,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.SPOT_HRV1_B1, Wavelengths.SPOT_HRV1_B2, Wavelengths.SPOT_HRV1_B3]
+    wv = [Wavelength.SPOT_HRV1_B1, Wavelength.SPOT_HRV1_B2, Wavelength.SPOT_HRV1_B3]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.545, 0.645, 0.84]
@@ -241,7 +241,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.SPOT_VGT_B1, Wavelengths.SPOT_VGT_B2, Wavelengths.SPOT_VGT_B3, Wavelengths.SPOT_VGT_B4]
+    wv = [Wavelength.SPOT_VGT_B1, Wavelength.SPOT_VGT_B2, Wavelength.SPOT_VGT_B3, Wavelength.SPOT_VGT_B4]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.45, 0.645, 0.835, 1.665]
@@ -262,7 +262,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.POLDER_B1, Wavelengths.POLDER_B2, Wavelengths.POLDER_B3, Wavelengths.POLDER_B4, Wavelengths.POLDER_B5, Wavelengths.POLDER_B6, Wavelengths.POLDER_B7, Wavelengths.POLDER_B8]
+    wv = [Wavelength.POLDER_B1, Wavelength.POLDER_B2, Wavelength.POLDER_B3, Wavelength.POLDER_B4, Wavelength.POLDER_B5, Wavelength.POLDER_B6, Wavelength.POLDER_B7, Wavelength.POLDER_B8]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.4445, 0.4449, 0.4922, 0.5645, 0.6702, 0.7633, 0.7631, 0.9077]
@@ -283,7 +283,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.SEAWIFS_B1, Wavelengths.SEAWIFS_B2, Wavelengths.SEAWIFS_B3, Wavelengths.SEAWIFS_B4, Wavelengths.SEAWIFS_B5, Wavelengths.SEAWIFS_B6, Wavelengths.SEAWIFS_B7, Wavelengths.SEAWIFS_B8]
+    wv = [Wavelength.SEAWIFS_B1, Wavelength.SEAWIFS_B2, Wavelength.SEAWIFS_B3, Wavelength.SEAWIFS_B4, Wavelength.SEAWIFS_B5, Wavelength.SEAWIFS_B6, Wavelength.SEAWIFS_B7, Wavelength.SEAWIFS_B8]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.412, 0.443, 0.49, 0.51, 0.555, 0.67, 0.765, 0.865]
@@ -304,7 +304,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.AATSR_B1, Wavelengths.AATSR_B2, Wavelengths.AATSR_B3, Wavelengths.AATSR_B4]
+    wv = [Wavelength.AATSR_B1, Wavelength.AATSR_B2, Wavelength.AATSR_B3, Wavelength.AATSR_B4]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.560, 0.660, 0.862, 1.594]
@@ -325,7 +325,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.ASTER_B1, Wavelengths.ASTER_B2, Wavelengths.ASTER_B3N, Wavelengths.ASTER_B3B, Wavelengths.ASTER_B4, Wavelengths.ASTER_B5, Wavelengths.ASTER_B6, Wavelengths.ASTER_B7, Wavelengths.ASTER_B8, Wavelengths.ASTER_B9]
+    wv = [Wavelength.ASTER_B1, Wavelength.ASTER_B2, Wavelength.ASTER_B3N, Wavelength.ASTER_B3B, Wavelength.ASTER_B4, Wavelength.ASTER_B5, Wavelength.ASTER_B6, Wavelength.ASTER_B7, Wavelength.ASTER_B8, Wavelength.ASTER_B9]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.56, 0.66, 0.81, 0.81, 1.65, 2.185, 2.205, 2.26, 2.33, 2.395]
@@ -346,7 +346,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.VIIRS_BM1, Wavelengths.VIIRS_BM2, Wavelengths.VIIRS_BM3, Wavelengths.VIIRS_BM4, Wavelengths.VIIRS_BM5, Wavelengths.VIIRS_BM6, Wavelengths.VIIRS_BM7, Wavelengths.VIIRS_BM8, Wavelengths.VIIRS_BM9, Wavelengths.VIIRS_BM10, Wavelengths.VIIRS_BM11, Wavelengths.VIIRS_BM12, Wavelengths.VIIRS_BI1, Wavelengths.VIIRS_BI2, Wavelengths.VIIRS_B3, Wavelengths.VIIRS_B4]
+    wv = [Wavelength.VIIRS_BM1, Wavelength.VIIRS_BM2, Wavelength.VIIRS_BM3, Wavelength.VIIRS_BM4, Wavelength.VIIRS_BM5, Wavelength.VIIRS_BM6, Wavelength.VIIRS_BM7, Wavelength.VIIRS_BM8, Wavelength.VIIRS_BM9, Wavelength.VIIRS_BM10, Wavelength.VIIRS_BM11, Wavelength.VIIRS_BM12, Wavelength.VIIRS_BI1, Wavelength.VIIRS_BI2, Wavelength.VIIRS_BI3, Wavelength.VIIRS_BI4]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.412, 0.445, 0.488, 0.555, 0.672, 0.746, 0.865, 1.24, 1.378, 1.61, 2.25, 3.70, 0.640, 0.865, 1.61, 3.74]
@@ -367,7 +367,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.ER2_MAS_B1, Wavelengths.ER2_MAS_B2, Wavelengths.ER2_MAS_B3, Wavelengths.ER2_MAS_B4, Wavelengths.ER2_MAS_B5, Wavelengths.ER2_MAS_B6, Wavelengths.ER2_MAS_B7]
+    wv = [Wavelength.ER2_MAS_B1, Wavelength.ER2_MAS_B2, Wavelength.ER2_MAS_B3, Wavelength.ER2_MAS_B4, Wavelength.ER2_MAS_B5, Wavelength.ER2_MAS_B6, Wavelength.ER2_MAS_B7]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.4649, 0.5494, 0.6550, 0.7024, 0.7431, 0.8248, 0.8667]
@@ -388,7 +388,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.ALI_B1P, Wavelengths.ALI_B1, Wavelengths.ALI_B2, Wavelengths.ALI_B3, Wavelengths.ALI_B4, Wavelengths.ALI_B4P, Wavelengths.ALI_B5P, Wavelengths.ALI_B5, Wavelengths.ALI_B7]
+    wv = [Wavelength.ALI_B1P, Wavelength.ALI_B1, Wavelength.ALI_B2, Wavelength.ALI_B3, Wavelength.ALI_B4, Wavelength.ALI_B4P, Wavelength.ALI_B5P, Wavelength.ALI_B5, Wavelength.ALI_B7]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.443, 0.4825, 0.565, 0.66, 0.79, 0.8675, 1.25, 1.65, 2.215]
@@ -409,7 +409,7 @@ class Wavelengths:
     A tuple containing the centre wavlengths used for the run and the results of the simulations. The results will be a list of :class:`SixS.Outputs` instances if ``output_name`` is not set,
     or a list of values of the selected output if ``output_name`` is set.
     """
-    wv = [Wavelengths.GLI_B1, Wavelengths.GLI_B2, Wavelengths.GLI_B3, Wavelengths.GLI_B4, Wavelengths.GLI_B5, Wavelengths.GLI_B6, Wavelengths.GLI_B7, Wavelengths.GLI_B8, Wavelengths.GLI_B9, Wavelengths.GLI_B10, Wavelengths.GLI_B11, Wavelengths.GLI_B12, Wavelengths.GLI_B13, Wavelengths.GLI_B14, Wavelengths.GLI_B15, Wavelengths.GLI_B16, Wavelengths.GLI_B17, Wavelengths.GLI_B18, Wavelengths.GLI_B19, Wavelengths.GLI_B20, Wavelengths.GLI_B21, Wavelengths.GLI_B22, Wavelengths.GLI_B23, Wavelengths.GLI_B24, Wavelengths.GLI_B25, Wavelengths.GLI_B26, Wavelengths.GLI_B27, Wavelengths.GLI_B28, Wavelengths.GLI_B29, Wavelengths.GLI_B30]
+    wv = [Wavelength.GLI_B1, Wavelength.GLI_B2, Wavelength.GLI_B3, Wavelength.GLI_B4, Wavelength.GLI_B5, Wavelength.GLI_B6, Wavelength.GLI_B7, Wavelength.GLI_B8, Wavelength.GLI_B9, Wavelength.GLI_B10, Wavelength.GLI_B11, Wavelength.GLI_B12, Wavelength.GLI_B13, Wavelength.GLI_B14, Wavelength.GLI_B15, Wavelength.GLI_B16, Wavelength.GLI_B17, Wavelength.GLI_B18, Wavelength.GLI_B19, Wavelength.GLI_B20, Wavelength.GLI_B21, Wavelength.GLI_B22, Wavelength.GLI_B23, Wavelength.GLI_B24, Wavelength.GLI_B25, Wavelength.GLI_B26, Wavelength.GLI_B27, Wavelength.GLI_B28, Wavelength.GLI_B29, Wavelength.GLI_B30]
     wv, res = cls.run_wavelengths(s, wv, output_name=output_name)
     
     centre_wvs = [0.380, 0.400, 0.412, 0.443, 0.460, 0.490, 0.520, 0.545, 0.565, 0.625, 0.666, 0.680, 0.678, 0.710, 0.710, 0.749, 0.763, 0.865, 0.865, 0.460, 0.545, 0.660, 0.825, 1.050, 1.135, 1.240, 1.380, 1.640, 2.210, 3.715]
@@ -428,11 +428,11 @@ class Wavelengths:
     
     Example usage::
     
-      SixSHelpers.Wavelengths.plot_wavelengths(wavelengths, values, 'Pixel Radiance (W/m^2)')
+      SixSHelpers.Wavelength.plot_wavelengths(wavelengths, values, 'Pixel Radiance ($W/m^2$)')
     
     """
     
     plot(wavelengths, values, 'k')
-    xlabel("Micrometres")
+    xlabel("Wavelength ($\mu m$)")
     ylabel(y_axis_label)
     show()
