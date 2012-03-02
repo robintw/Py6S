4ba3c68b887215650a07bbe42c326528272f8bbb
Robin Wilson <robin@rtwilson.com>
2012-03-02 15:26:46 +0000

    Essential update to the setup.py script to bring the SixSHelpers into the package!

--- Py6S/SixSHelpers/__init__.py
+++ Py6S/SixSHelpers/__init__.py
@@ -1,3 +1,5 @@
+"""Contains a number of classes for helper methods"""
+
 from all_angles import *
 from all_wavelengths import *
 from radiosonde import *
--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ def read(fname):
 
 setup(
     name = "Py6S",
-    packages = ['Py6S', 'Py6S.Params'],
+    packages = ['Py6S', 'Py6S.Params', 'Py6S.SixSHelpers'],
     requires = ['yaml'],
-    version = "0.6",
+    version = "0.62",
     author = "Robin Wilson",
     author_email = "robin@rtwilson.com",
     description = ("""A wrapper for the 6S Radiative Transfer Model to make it easy to run simulations
