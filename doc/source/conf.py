b71392935ee8dcd4740959e82f5d9fb67007e0a6
Robin Wilson <robin@rtwilson.com>
2012-08-09 09:21:58 +0200

    Changed to use Mock module

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,23 +12,7 @@
 # serve to show the default.
 
 import sys, os
-
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            return type(name, (), {})
-        else:
-            return Mock()
+import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
