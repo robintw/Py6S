383467ebc42e29867b5fa60ca3d27533d970bd84
Robin Wilson <robin@rtwilson.com>
2012-08-09 09:27:31 +0200

    Update doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@
 import sys, os
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
