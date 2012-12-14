d45a45a26e2587027550f2e943049d920e7b5d9e
Robin Wilson <robin@rtwilson.com>
2012-12-14 21:52:41 +0000

    Added proper licensing info #SSI_Alterations
    Added COPYING and COPYING.LESSER file, as required for LGPL license
    Added header to all files with copyright info and license info

--- test/test.py
+++ test/test.py
@@ -1,3 +1,20 @@
+# This file is part of Py6S.
+#
+# Copyright 2012 Robin Wilson and contributors listed in the CONTRIBUTORS file.
+#
+# Py6S is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Py6S is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Py6S.  If not, see <http://www.gnu.org/licenses/>.
+
 import unittest
 from Py6S import *
 import numpy as np
