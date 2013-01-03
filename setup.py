8df5e473e0c79718b1db4bc82f244c6f1a7a7866
Robin Wilson <robin@rtwilson.com>
2013-01-03 14:56:04 +0000

    Bumped version to 1.2.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         packages              = ['Py6S', 'Py6S.Params', 'Py6S.SixSHelpers'],
         requires              = ['pysolar', 'numpy', 'matplotlib', 'scipy'],
         install_requires      = ['pysolar', 'numpy', 'matplotlib', 'scipy'],
-        version               = "1.2",
+        version               = "1.2.1",
         author                = "Robin Wilson",
         author_email          = "robin@rtwilson.com",
         description           = ("""A wrapper for the 6S Radiative Transfer Model to make it easy to run simulations
