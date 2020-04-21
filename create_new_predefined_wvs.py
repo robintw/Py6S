7a6da6e88342069d13d0270901cb80c6ba592da4
Robin Wilson <robin@rtwilson.com>
2020-04-21 20:22:49 +0100

    Change .ix to .iloc or .loc as .ix now deprecated

--- create_new_predefined_wvs.py
+++ create_new_predefined_wvs.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 def import_from_excel(filename, sheetid):
     df = pd.read_excel(filename, sheetid)
-    f = interp1d(df.ix[:, 0], df.ix[:, 1])
-    minwv = df.ix[:, 0].min()
+    f = interp1d(df.iloc[:, 0], df.iloc[:, 1])
+    minwv = df.iloc[:, 0].min()
 
-    newwvs = np.arange(minwv, df.ix[:, 0].max(), 2.5)
+    newwvs = np.arange(minwv, df.iloc[:, 0].max(), 2.5)
 
     maxwv = newwvs.max()
 
@@ -16,19 +16,20 @@ def import_from_excel(filename, sheetid):
 
     print("%.3f, %.3f,\nnp.%s)" % (minwv / 1000.0, maxwv / 1000.0, values.__repr__()))
 
+
 def process_band(df, bandid):
-	data = df[[0, bandid]]
-	data.columns = [0, 1]
-	f = interp1d(data.ix[:, 0], data.ix[:, 1])
+    data = df[[0, bandid]]
+    data.columns = [0, 1]
+    f = interp1d(data.iloc[:, 0], data.iloc[:, 1])
 
-	indices = np.where(data.ix[:, 1] > 0.001)[0]
-	print(indices)
-	minwv = data.ix[indices.min(), 0]
-	maxwv = data.ix[indices.max(), 0]
-	newwvs = np.arange(minwv, maxwv, 2.5)
+    indices = np.where(data.iloc[:, 1] > 0.001)[0]
+    print(indices)
+    minwv = data.iloc[indices.min(), 0]
+    maxwv = data.iloc[indices.max(), 0]
+    newwvs = np.arange(minwv, maxwv, 2.5)
 
-	values = f(newwvs)
+    values = f(newwvs)
 
-	values[values < 0.001] = 0.0
+    values[values < 0.001] = 0.0
 
-	print("%.3f, %.3f,\nnp.%s)" % (minwv / 1000.0, maxwv / 1000.0, values.__repr__()))
+    print("%.3f, %.3f,\nnp.%s)" % (minwv / 1000.0, maxwv / 1000.0, values.__repr__()))
